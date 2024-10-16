bd59724447952a3b0f4143b761748ed8067afea3
Fabricio3916 <fabriciopassos3916@gmail.com>
2024-10-16 15:41:21 -0300

    finalizado o jogo

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -25,7 +25,7 @@ for(let i = 0; i < emojis.length; i++){
     let box = document.createElement("div");
     box.className = "item";
     box.innerHTML = shuffleEmojis[i];
-    box.onclick = handleClick();
+    box.onclick = handleClick;
     document.querySelector(".game").appendChild(box);
 }
 
@@ -41,5 +41,17 @@ function handleClick(){
 }
 
 function checkMatch(){
+    if(openCards[0].innerHTML === openCards[1].innerHTML){
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    }else{
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
 
+    openCards = [];
+
+    if(document.querySelectorAll(".boxMatch").length === emojis.length){
+        alert("Você venceu !");
+    }
 }
