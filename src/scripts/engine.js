e8762a71a7645291f33b377c97b65e3a8cc8ea02
Fabricio3916 <fabriciopassos3916@gmail.com>
2024-10-16 14:40:15 -0300

    Criado mais funções no javascript

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,4 +1,4 @@
-const emoji = [
+const emojis = [
     "🐱‍🐉",
     "🐱‍🐉",
     "🎶",
@@ -7,15 +7,39 @@ const emoji = [
     "😎",
     "🐱‍🏍",
     "🐱‍🏍",
-    "🐱‍🚀",
-    "🐱‍🚀",
-    "🎁",
-    "🎁",
     "🎉",
     "🎉",
     "🎂",
-    "🎂"
+    "🎂",
+    "🎨",
+    "🎨",
+    "🍔",
+    "🍔"
 ];
+
 let openCards = [];
 
-let shuffleEmojis = emoji.sort(() =>(Math.random() > 0.5) ? 2 : -1);
+let shuffleEmojis = emojis.sort(() =>(Math.random() > 0.5 ? 2 : -1));
+
+for(let i = 0; i < emojis.length; i++){
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick();
+    document.querySelector(".game").appendChild(box);
+}
+
+function handleClick(){
+    if(openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if(openCards.length == 2){
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch(){
+
+}
